08e67553d245b74a6bccaee25c0625cbd54b139c
juanse021 <juanse032194@hotmail.com>
2017-10-13 22:49:28 -0500

    Fix

---
+++ Parcial I/constantMemory/constantMemory.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#SBATCH --job-name=constantMemory
+#SBATCH --output=constantMemory.out
+#SBATCH --ntasks=1
+#SBATCH --nodes=1
+#SBATCH --gres=gpu:1
+
+export PATH=/usr/local/cuda-8.0/bin${PATH:+:${PATH}}
+export LD_LIBRARY_PATH=/usr/local/cuda-8.0/lib64/${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}
+
+export CUDA_VISIBLE_DEVICES=0
+
+./constantMemory ../Images/ferrari.jpg
